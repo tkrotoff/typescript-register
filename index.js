ec46ab007a924297e40a56af00b012385fc15ae8
Tanguy Krotoff <tkrotoff@gmail.com>
2015-07-07 01:06:22 +0200

    Fix process.cwd(), see https://github.com/pspeter3/typescript-register/issues/10

--- index.js
+++ index.js
@@ -2,28 +2,27 @@
 /// <reference path="typings/chalk/chalk.d.ts"/>
 /// <reference path="typings/node/node.d.ts"/>
 /// <reference path="typings/sanitize-filename/sanitize-filename.d.ts"/>
-import chalk = require("chalk");
-import fs = require("fs");
-import os = require("os");
-import path = require("path");
-import sanitize = require("sanitize-filename");
-import typescript = require("typescript");
-
+var chalk = require("chalk");
+var fs = require("fs");
+var os = require("os");
+var path = require("path");
+var sanitize = require("sanitize-filename");
+var typescript = require("typescript");
 /**
  * Module Configuration Options
  */
-enum Config {
-    EMIT_ERROR,
-    USE_CACHE,
-    COMPILER_OPTIONS
-}
-
+var Config;
+(function (Config) {
+    Config[Config["EMIT_ERROR"] = 0] = "EMIT_ERROR";
+    Config[Config["USE_CACHE"] = 1] = "USE_CACHE";
+    Config[Config["COMPILER_OPTIONS"] = 2] = "COMPILER_OPTIONS";
+})(Config || (Config = {}));
 /**
  * Requires a TypeScript file
  * @param {Module} module   The node module
  * @param {string} filename The module filename
  */
-function req(module: Module, filename: string): void {
+function req(module, filename) {
     var options = compilerOptions();
     var out = dest(filename, options);
     if (!useCache() || isModified(filename, out)) {
@@ -31,59 +30,51 @@ function req(module: Module, filename: string): void {
     }
     module._compile(fs.readFileSync(out, "utf8"), filename);
 }
-
 require.extensions[".ts"] = req;
-
 /**
  * Checks the environment for whether or not we should emit an error. Defaults
  * to true.
  * @return {boolean} Whether or not to emit TypeScript errors
  */
-function emitError(): boolean {
+function emitError() {
     return env(Config.EMIT_ERROR, true, toBoolean);
 }
-
 /**
  * Checks the environment for whether or not we should use a build cache.
  * Defaults to true.
  * @return {boolean} Whether or not to use cached JavaScript files
  */
-function useCache(): boolean {
+function useCache() {
     return env(Config.USE_CACHE, true, toBoolean);
 }
-
 /**
  * Returns path to cache for source directory.
  * @param  {string} directory Directory with source code
  * @return {string}           Path with all special characters replaced with _ and
- *                            prepended path to temporary directory 
+ *                            prepended path to temporary directory
  */
-function getCachePath(directory: string): string {
+function getCachePath(directory) {
     var sanitizeOptions = {
         replacement: "_"
     };
-    var parts = directory.split(path.sep).map((p) => sanitize(p, sanitizeOptions));
+    var parts = directory.split(path.sep).map(function (p) { return sanitize(p, sanitizeOptions); });
     var cachePath = path.join.apply(null, parts);
-
     return path.join(os.tmpdir(), "typescript-register", cachePath);
 }
-
 /**
  * Checks the environment for JSON stringified compiler options. By default it
  * will compile TypeScript files into a scoped temp directory using ES5 and
  * CommonJS targets.
  * @return {typescript.CompilerOptions} The TypeScript compiler settings
  */
-function compilerOptions(): typescript.CompilerOptions {
-    var defaultCompilerOptions: typescript.CompilerOptions = {
+function compilerOptions() {
+    var defaultCompilerOptions = {
         module: typescript.ModuleKind.CommonJS,
         outDir: getCachePath(process.cwd()),
         target: typescript.ScriptTarget.ES5
     };
-
     return env(Config.COMPILER_OPTIONS, defaultCompilerOptions, toOptions);
 }
-
 /**
  * Gets a value from env
  * @param  {Config}   config   The configuration target
@@ -91,48 +82,46 @@ function compilerOptions(): typescript.CompilerOptions {
  * @param  {Function} map      The map function if the value exists
  * @return {T}                 The environment variable
  */
-function env<T>(config: Config, fallback: T, map: (value: string) => T): T {
-    var key: string = "TYPESCRIPT_REGISTER_" + Config[config];
+function env(config, fallback, map) {
+    var key = "TYPESCRIPT_REGISTER_" + Config[config];
     if (process.env.hasOwnProperty(key)) {
         return map(process.env[key]);
     }
     return fallback;
 }
-
 /**
  * Returns the JavaScript destination for the path
  * @param  {string}                      filename The TypeScript filename
  * @param  {typescript.CompilerOptiones} options  The Compiler Options
  * @return {string}                               The JavaScript filepath
  */
-function dest(filename: string, options: typescript.CompilerOptions): string {
+function dest(filename, options) {
     var basename = path.basename(filename, ".ts");
     var outDir = options.outDir || path.dirname(filename);
     return path.join(outDir, basename + ".js");
 }
-
 /**
  * Check whether or not the path is modified
  * @param  {string}  tsPath The path to the TypeScript file
  * @param  {string}  jsPath The path to the JavaScript file
  * @return {boolean}        Whether or not the file is modified
  */
-function isModified(tsPath: string, jsPath: string): boolean {
+function isModified(tsPath, jsPath) {
     try {
         var js = fs.statSync(jsPath).mtime;
         var ts = fs.statSync(tsPath).mtime;
         return ts > js;
-    } catch (err) {
+    }
+    catch (err) {
         return true;
     }
 }
-
 /**
  * Compiles the TypeScript file
  * @param {string}                     filename The root file to compile
  * @param {typescript.CompilerOptions} options  The Compiler Options
  */
-function compile(filename: string, options: typescript.CompilerOptions): void {
+function compile(filename, options) {
     var host = typescript.createCompilerHost(options);
     var program = typescript.createProgram([filename], options, host);
     var checker = program.getTypeChecker(true);
@@ -143,55 +132,34 @@ function compile(filename: string, options: typescript.CompilerOptions): void {
             .concat(result.diagnostics)));
     }
 }
-
 /**
  * Converts a list of errors into something readable
  * @param  {typescript.Diagnostic[]} errors The TypeScript Diagnostics
  * @return {Error}                          The Compiler Error
  */
-function checkErrors(errors: typescript.Diagnostic[]): void {
+function checkErrors(errors) {
     if (errors.length === 0) {
         return;
     }
-    errors.forEach((diagnostic: typescript.Diagnostic): void => {
-        var position = diagnostic.file.getLineAndCharacterFromPosition(
-            diagnostic.start);
-        console.error(
-            chalk.bgRed("" + diagnostic.code),
-            chalk.grey(`${diagnostic.file.filename}, (${position.line},${position.character})`),
-            diagnostic.messageText);
+    errors.forEach(function (diagnostic) {
+        var position = diagnostic.file.getLineAndCharacterFromPosition(diagnostic.start);
+        console.error(chalk.bgRed("" + diagnostic.code), chalk.grey(diagnostic.file.filename + ", (" + position.line + "," + position.character + ")"), diagnostic.messageText);
     });
     throw new Error("TypeScript Compilation Errors");
 }
-
 /**
  * Converts a string to boolean
  * @param  {string}  value The string value
  * @return {boolean}       Whether or not the string is "true"
  */
-function toBoolean(value: string): boolean {
+function toBoolean(value) {
     return value === "true";
 }
-
 /**
  * Converts a string to TypeScript compiler options
  * @param  {string}                     value The string value
  * @return {typescript.CompilerOptions}       The TypeScript Compiler Options
  */
-function toOptions(value: string): typescript.CompilerOptions {
-    return <typescript.CompilerOptions>JSON.parse(value);
-}
-
-/**
- * The Module passed in by Node
- */
-interface Module {
-    id: string;
-    exports: any;
-    parent: Module;
-    filename: string;
-    loaded: boolean;
-    children: Module[];
-    paths: string[];
-    _compile: (src: string, filename: string) => void;
+function toOptions(value) {
+    return JSON.parse(value);
 }
